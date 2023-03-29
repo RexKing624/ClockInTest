367e1aef9429e6cde48726c4630b6da1b860d52e
RexKing624 <RexKing624@gamil.com>
2023-03-30 04:12:23 +0800

    首页test

--- WebProject/src/components/HelloWorld.vue
+++ WebProject/src/components/HelloWorld.vue
@@ -1,43 +1,51 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
+<!--<script setup>-->
+<!--defineProps({-->
+<!--  msg: {-->
+<!--    type: String,-->
+<!--    required: true-->
+<!--  },-->
+<!--    mssg: {-->
+<!--        type: String,-->
+<!--        required: true-->
+<!--    }-->
+<!--})-->
 
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
-</template>
+<!--</script>-->
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<!--<template>-->
+<!--  <div class="greetings">-->
+<!--    <h1 class="green">{{ msg }}</h1>-->
+<!--    <h1 class="green">{{ mssg }}</h1>-->
 
-h3 {
-  font-size: 1.2rem;
-}
+<!--    <h3>-->
+<!--      You’ve successfully created a project with-->
+<!--      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +-->
+<!--      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.-->
+<!--        <button v-text="123" ></button>-->
+<!--    </h3>-->
+<!--  </div>-->
+<!--</template>-->
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+<!--<style scoped>-->
+<!--h1 {-->
+<!--  font-weight: 500;-->
+<!--  font-size: 2.6rem;-->
+<!--  top: -10px;-->
+<!--}-->
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<!--h3 {-->
+<!--  font-size: 1.2rem;-->
+<!--}-->
+
+<!--.greetings h1,-->
+<!--.greetings h3 {-->
+<!--  text-align: center;-->
+<!--}-->
+
+<!--@media (min-width: 1024px) {-->
+<!--  .greetings h1,-->
+<!--  .greetings h3 {-->
+<!--    text-align: left;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
