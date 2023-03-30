5689b0671c58b85952610d4a5b441e18e64b2503
RexKing624 <RexKing624@gamil.com>
2023-03-31 00:50:05 +0800

    updata

--- WebProject/src/App.vue
+++ WebProject/src/App.vue
@@ -1,22 +1,24 @@
 <template>
-<div class="Up">
-    <clockPage btuName="test" @click="sumit"/>
-</div>
+    <div class="Up">
+        <clockPage btuName="test" @click="sumit"/>
+    </div>
 
-<div class="Down">
-    <clockPage btuName="123"></clockPage>
-</div>
+    <div class="Down">
+        <clockPage btuName="123"></clockPage>
+    </div>
 </template>
 
 <script setup>
-import clockPage from './components/clockPage.vue'
+import clockPage from './components/clockUp.vue'
+
+
 </script>
 <style scoped>
-    .Up{
-        color: red;
-            /*background-color: red;*/
-            color: red;
-            /*margin-left: 10px;*/
-    }
+    /*.Up{*/
+    /*    color: red;*/
+    /*        !*background-color: red;*!*/
+    /*        color: red;*/
+    /*        !*margin-left: 10px;*!*/
+    /*}*/
 
 </style>
---
+++ WebProject/src/components/clockUp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+<!--      <h1>{{ msg }}</h1>-->
+      <el-row class="rowBox">
+          <el-button class="btu" @click="submit()">{{ btuName }}</el-button>
+<!--          <el-button class="btu" @click="submit()">test</el-button>-->
+      </el-row>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        btuName: String,
+    },
+    data(){
+        return{
+        }
+    },
+    mounted() {
+        console.log('-------===-------')
+    },
+    methods:{
+        submit(){
+            console.log(this.btuName,'-----')
+            // var timestamp = Date.parse(new Date());
+            // console.log(timestamp,'=====')
+            var myDate = new Date();
+            // var Yer=myDate.getMonth();
+            var timeStamp = myDate.getTime()
+            var newDate = new Date(timeStamp);
+            console.log(timeStamp)
+
+        },
+
+
+    },
+
+}
+</script>
+
+<style scoped>
+ .rowBox{
+     .btu{
+         background-color: red;
+         color: red;
+         margin-left: 1000px;
+     }
+ }
+</style>
